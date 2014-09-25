182ba24976789a98facaf67fa633c058efe32262
noyobo <nongyoubao@alibaba-inc.com>
2014-09-25 21:14:00 +0800

    snippets gulp test

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+var gulp = require('gulp');
+var lintspaces = require("gulp-lintspaces");
+var map = require('map-stream');
+var gutil = require('gulp-util');
+var path = require('path');
+var _ = require('lodash');
+
+
+var tabArr = [];
+// check tabTrigger
+var checkDuplicate = function(file, cb) {
+    var regRex = /tabTrigger\>(\w+)</m;
+    var baseName = path.basename(file.path)
+    var tabName = String(file.contents).match(regRex)[1];
+    if (!tabName) {
+        gutil.log(gutil.colors.red("tabTrigger not found: ", baseName));
+    };
+    if (_.indexOf(tabArr, tabName) !== -1) {
+        gutil.log(gutil.colors.red("tabTrigger[" + tabName + "] is Duplicate: ", baseName));
+    };
+    tabArr.push(tabName)
+    cb(null, file)
+}
+
+gulp.task("lint", function() {
+    return gulp
+        .src("*.sublime-snippet")
+        .pipe(lintspaces({
+            editorconfig: '.editorconfig'
+        }))
+        .pipe(lintspaces.reporter());
+});
+
+gulp.task('check', function() {
+    return gulp
+        .src("*.sublime-snippet")
+        .pipe(map(checkDuplicate))
+});
+
+gulp.task('default', function() {
+    gulp.run(['lint', 'check']);
+});
