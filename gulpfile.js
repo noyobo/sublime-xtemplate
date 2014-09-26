1d8f9cd427413cda32a1250bfe622311ca042552
noyobo <nongyoubao@alibaba-inc.com>
2014-09-26 11:38:40 +0800

    gulp dev

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,18 @@ var map = require('map-stream');
 var gutil = require('gulp-util');
 var path = require('path');
 var _ = require('lodash');
+var copy = require('gulp-copy')
 
+
+var paths = {
+    snipptes: '**/*.sublime-snippet',
+    language: '**/*.tmLanguage'
+}
 
 var tabArr = [];
 // check tabTrigger
 var checkDuplicate = function(file, cb) {
-    var regRex = /tabTrigger\>(\w+)</m;
+    var regRex = /tabTrigger\>([\+\w]+)</m;
     var baseName = path.basename(file.path)
     var tabName = String(file.contents).match(regRex)[1];
     if (!tabName) {
@@ -25,7 +31,7 @@ var checkDuplicate = function(file, cb) {
 
 gulp.task('lint', function() {
     return gulp
-        .src("*.sublime-snippet")
+        .src(paths.snipptes)
         .pipe(lintspaces({
             editorconfig: '.editorconfig'
         }))
@@ -34,8 +40,14 @@ gulp.task('lint', function() {
 
 gulp.task('check', function() {
     return gulp
-        .src("*.sublime-snippet")
+        .src(paths.snipptes)
         .pipe(map(checkDuplicate))
 });
+
+gulp.task('dev', function() {
+    return gulp
+        .src([paths.snipptes, paths.language])
+        .pipe(copy('C:/Users/Administrator/AppData/Roaming/Sublime Text 3/Packages/User'))
+})
 
 gulp.task('default', ['lint', 'check']);
